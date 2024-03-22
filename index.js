20105378ad2e4bdf35736c69ff9e21635ee54e6a
Manav Misra <manavm@visionwebsoft.com>
2024-03-22 18:23:19 -0500

    Refactor code to use const for variables and update function parameters

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-//Declare variable name
-var name = "Kelan"
+const userName = "Kelan";
+const userAge = 21;
 
-//Declare variable age
-var age = 21
-
-//Write verifyAdulthood function 
 function verifyAdulthood(age, name) {
-    if (age >=18) {
-        return `Welcome, ${name}!`
-    } else {
-        return `You are not old enough to enter, ${name}`
-    }
+  if (age >= 18) {
+    return `Welcome, ${name}!`;
+  }
+  return `You are not old enough to enter, ${name}`;
 }
-console.log(verifyAdulthood(age,name))
+
+console.log(verifyAdulthood(userAge, userName));
