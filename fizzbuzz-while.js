8f8deb2f75696d12de8661224943e77c3c4c41fa
ScrubC0der <rorkedraws@gmail.com>
2024-03-26 20:22:02 -0500

    Added && operators for Fizzbuzz multiples

--- fizzbuzz-while.js
+++ fizzbuzz-while.js
@@ -5,7 +5,7 @@ while (i <= 100) {
   i += 1;
   if (i % 3 == 0) console.log("Fizz");
   else if (i % 5 == 0) console.log("Buzz");
-  else if (i % 3 == 0) i % 5 == 0;
+  else if (i % 3 == 0 && i % 5 == 0);
   console.log("Fizzbuzz");
   console.log(i);
 }
