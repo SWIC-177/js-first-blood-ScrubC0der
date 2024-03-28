c45ad13697854a8159eff9203d94505963eee0fb
ScrubC0der <rorkedraws@gmail.com>
2024-03-28 17:44:39 -0500

    Used object literals to define values using dot notations

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const userName = "Kelan";
-const userAge = 21;
+const person = {
+  name: "John",
+  age: 18,
+};
 
-function verifyAdulthood(age, name) {
-  if (age >= 18) {
-    return `Welcome, ${name}!`;
-  }
-  return `You are not old enough to enter, ${name}`;
+function verifyAdulthood(personName, personAge) {
+  if (person.age >= 18) return `Welcome, ${person.name}!`;
+
+  return `You are not old enough to enter, ${person.name}.`;
 }
 
-console.log(verifyAdulthood(userAge, userName));
+const adultMessage = verifyAdulthood(person.name, person.age);
+
+console.log(adultMessage);
