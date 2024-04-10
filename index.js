f9e6dca299bb9834b1618baed8adcc8c3d4dbbec
ScrubC0der <rorkedraws@gmail.com>
2024-04-09 19:07:47 -0500

    Copied assignment to index

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
-const person = {
-  name: "John Doe",
-  age: 23,
-  street: "123 Main St",
-};
+const people = [
+  {
+    id: 1,
+    name: "John Doe",
+    age: 23,
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    age: 29,
+  },
+  {
+    id: 3,
+    name: "Jim Doe",
+    age: 34,
+  },
+  {
+    id: 4,
+    name: "Jill Doe",
+    age: 45,
+  },
+  {
+    id: 5,
+    name: "Jack Doe",
+    age: 56,
+  },
+  {
+    id: 6,
+    name: "Jenny Doe",
+    age: 67,
+  },
+];
 
-function updatePersonName(p, newName) {
-  const p2Update = { ...p };
+function renameOdds(p) {
+  for (let i = 0; i < p.length; i++) {
+    if (p[i].id % 2 !== 0) {
+      p[i].name = "Odd Name";
+    }
+  }
 
-  p2Update.name = newName;
   return p;
 }
 
-const updatedPerson = updatePersonName(person, "Jane Doe");
-console.log(updatedPerson);
+console.log(renameOdds(people));
